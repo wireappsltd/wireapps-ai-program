cf01b4ad10820ff8deb681057ab7eeccb88a1e89
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2026-03-20 01:28:47 +0530

    add OPENAI_API_KEY to settings

--- app/settings.py
+++ app/settings.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
   ANTHROPIC_API_KEY: str = ""
+  OPENAI_API_KEY: str = ""
 
   model_config = SettingsConfigDict(env_file=".env")
 
